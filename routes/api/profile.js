78443730a0a24eee7376ae014fb2351817721f6b
Irfan Ullah Munna <shantocuet659@gmail.com>
2021-06-01 11:07:01 +0600

    get all profiles and profile by userid

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
-// const { check, validationResult } = require('express-validator/check');
-const { check } = require('express-validator');
+const { check, validationResult } = require('express-validator/check');
+// const { check } = require('express-validator');
 
 
 
@@ -123,6 +123,30 @@ router.post('/',
 
         }
     }
-)
+);
+
+// @route       GET api/profile/user/:user_id
+// @desc        Get profile by user ID
+// @access      Public
+router.get('/user/:user_id', async (req, res) => {
+    try {
+        const profile = await Profile.findOne({ user: req.params.user_id }).populate(
+            'user', 
+            ['name', 'avatar']
+        );
+
+        if(!profile) return res.status(400).json({ msg: 'Profile not  found' });
+
+        res.json(profile);
+
+    } catch(err) {
+        console.error(err.message);
+        if(err.kind == 'ObjectId') {
+            return res.status(400).json({ msg: 'Profile not found' });
+        }
+        res.status(500).send('Server Error');
+    }
+})
+
 
 module.exports = router;
